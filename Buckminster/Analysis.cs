e1652b26cbf0a1b80056d481707fb2ce63e7796f
Will Pearson <pearswj@gmail.com>
2013-05-11 16:48:57 +0100

    add simple stiffness method analysis and tests (see #4)
    NOTE: set up for unit tests, not for Grasshopper

---
+++ Buckminster/Analysis.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//using Rhino.Geometry;
+using System.Windows.Media.Media3D;
+using Vector3d = System.Windows.Media.Media3D.Vector3D;
+using Point3d = System.Windows.Media.Media3D.Point3D;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace Buckminster
+{
+    public class Analysis
+    {
+        public static bool StiffnessMethod(
+            IList<Point3d> nodes,
+            IList<int> bars_s, // indices of start nodes
+            IList<int> bars_e, // indices of end nodes
+            IList<double> bars_mat, // csa, for now
+            IList<Vector3d> loads,
+            IList<Vector3d> supports_t, // translational fixities
+            //IList<Vector3d> supports_r, // rotational fixities (not yet implemented)
+            out Vector3d[] deflections
+            )
+        {
+            var num_bars = bars_s.Count;
+            var num_nodes = nodes.Count;
+
+            if (num_nodes != loads.Count |
+                num_nodes != supports_t.Count |
+                num_bars != bars_e.Count |
+                num_bars != bars_mat.Count)
+            {
+                deflections = new Vector3d[num_nodes];
+                return false;
+            }
+
+            var YoungsModulus = 205000.0;
+
+            //var matrix = new DenseMatrix(n * 2, n * 2);
+            var stiffness_matrix = new double[num_nodes * 2, num_nodes * 2];
+
+            // build stiffness matrix
+
+            for (int i = 0; i < num_bars; i++)
+            {
+                var vector = nodes[bars_e[i]] - nodes[bars_s[i]];
+                var EA_over_Lcubed = YoungsModulus * bars_mat[i] / Math.Pow(vector.Length, 3); // bar_mat is csa
+                var xx_stiffness = EA_over_Lcubed * Math.Pow(vector.X, 2);
+                var yy_stiffness = EA_over_Lcubed * Math.Pow(vector.Y, 2);
+                var xy_stiffness = EA_over_Lcubed * vector.X * vector.Y;
+
+                var start_dof = 2 * bars_s[i];
+                var end_dof = 2 * bars_e[i];
+
+                stiffness_matrix[start_dof, start_dof] += xx_stiffness;
+                stiffness_matrix[end_dof, end_dof] += xx_stiffness;
+                stiffness_matrix[start_dof, end_dof] -= xx_stiffness;
+                stiffness_matrix[end_dof, start_dof] -= xx_stiffness;
+
+                stiffness_matrix[start_dof + 1, start_dof + 1] += yy_stiffness;
+                stiffness_matrix[end_dof + 1, end_dof + 1] += yy_stiffness;
+                stiffness_matrix[start_dof + 1, end_dof + 1] -= yy_stiffness;
+                stiffness_matrix[end_dof + 1, start_dof + 1] -= yy_stiffness;
+
+                stiffness_matrix[start_dof + 1, start_dof] += xy_stiffness;
+                stiffness_matrix[end_dof + 1, end_dof] += xy_stiffness;
+                stiffness_matrix[start_dof + 1, end_dof] -= xy_stiffness;
+                stiffness_matrix[end_dof + 1, start_dof] -= xy_stiffness;
+
+                stiffness_matrix[start_dof, start_dof + 1] += xy_stiffness;
+                stiffness_matrix[end_dof, end_dof + 1] += xy_stiffness;
+                stiffness_matrix[start_dof, end_dof + 1] -= xy_stiffness;
+                stiffness_matrix[end_dof, start_dof + 1] -= xy_stiffness;
+            }
+
+            // apply boundary conditions
+
+            for (int i = 0; i < num_nodes; i++)
+            {
+                var x_dof = 2 * i;
+                var y_dof = x_dof + 1;
+
+                if (supports_t[i].X == 1) // x-fixed
+                {
+                    for (int dof_count = 0; dof_count < num_nodes * 2; dof_count++)
+                    {
+                        stiffness_matrix[x_dof, dof_count] = 0.0;
+                        stiffness_matrix[dof_count, x_dof] = 0.0;
+                    }
+                    stiffness_matrix[x_dof, x_dof] = 1.0;
+
+                    // set x-load to zero
+                    loads[i] = new Vector3d(0.0, loads[i].Y, loads[i].Z);
+                }
+
+                if (supports_t[i].Y == 1) // y-fixed
+                {
+                    for (int dof_count = 0; dof_count < num_nodes * 2; dof_count++)
+                    {
+                        stiffness_matrix[y_dof, dof_count] = 0.0;
+                        stiffness_matrix[dof_count, y_dof] = 0.0;
+                    }
+                    stiffness_matrix[y_dof, y_dof] = 1.0;
+
+                    // set y-load to zero
+                    loads[i] = new Vector3d(loads[i].X, 0.0, loads[i].Z);
+                }
+            }
+
+            var inverse_stiffness_matrix = DenseMatrix.OfArray(stiffness_matrix).Inverse();
+
+            var force_vector_storage = new double[num_nodes * 2];
+            var force_vector = new DenseVector(force_vector_storage);
+            for (int i = 0; i < num_nodes; i++)
+            {
+                force_vector_storage[2 * i] = loads[i].X;
+                force_vector_storage[2 * i + 1] = loads[i].Y;
+            }
+
+            var deflections_vector = inverse_stiffness_matrix.Multiply(force_vector);
+
+            var deflections_array = deflections_vector.ToArray();
+
+            deflections = new Vector3d[num_nodes];
+            for (int i = 0; i < num_nodes; i++)
+            {
+                deflections[i] = new Vector3d(deflections_vector[2 * i], deflections_vector[2 * i + 1], 0.0);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ BuckminsterTest/AnalysisTests.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Windows.Media.Media3D;
+using Buckminster;
+
+namespace BuckminsterTest
+{
+    [TestClass]
+    public class AnalysisTests
+    {
+        [TestMethod]
+        public void TestSimpleStiffnessMethod()
+        {
+            // Set-up model (see Pinned2D - Nodes_X.txt and Lines_X.txt)
+
+            // nodes
+            var nodes_temp = new double[,]{
+                {0.0, 0.0},
+                {1.0, 0.0},
+                {1.0, 1.0},
+                {0.0, 1.0}
+            };
+            int n = nodes_temp.GetLength(0);
+            Point3D[] nodes = new Point3D[n];
+            for (int i = 0; i < n; i++)
+            {
+                nodes[i] = new Point3D(nodes_temp[i, 0], nodes_temp[i, 1], 0.0);
+            }
+
+            // bars
+            var bars_s = new int[] { 0, 1, 2, 3, 0, 1 };
+            var bars_e = new int[] { 1, 2, 3, 0, 2, 3 };
+            var bars_mat = new double[] { 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 };
+
+            // loads
+            var loads_temp = new double[,] {
+                {0.0, 0.0},
+                {0.0, 0.0},
+                {0.0, 0.0},
+                {20000.0, 0.0}
+            };
+            Vector3D[] loads = new Vector3D[n];
+            for (int i = 0; i < n; i++)
+            {
+                loads[i] = new Vector3D(loads_temp[i, 0], loads_temp[i, 1], 0.0);
+            }
+
+            // supports
+            var supports_temp = new double[,] {
+                {1, 1},
+                {0, 1},
+                {0, 0},
+                {0, 0}
+            };
+            Vector3D[] supports = new Vector3D[supports_temp.GetLength(0)];
+            for (int i = 0; i < supports.Length; i++)
+            {
+                supports[i] = new Vector3D(supports_temp[i, 0], supports_temp[i, 1], 0.0);
+            }
+
+            // Call the method
+
+            Vector3D[] displacements;
+            Buckminster.Analysis.StiffnessMethod(nodes, bars_s, bars_e, bars_mat, loads, supports, out displacements);
+
+            var target_displacements = new double[] {
+                0.0, 0.0,
+                0.04878051,0.0,
+                0.18675262,-0.048780516,
+                0.23553312,0.048780512
+            };
+
+            var results = new double[n * 2];
+            for (int i = 0; i < n; i++)
+            {
+                results[i * 2] = displacements[i].X;
+                results[i * 2 + 1] = displacements[i].Y;
+            }
+
+            // compare results (to a tolerance)
+            
+            for (int i = 0; i < n * 2; i++)
+            {
+                Assert.AreEqual(target_displacements[i], results[i], 1E-06);
+            }
+        }
+
+        [TestMethod]
+        public void TestSimpleStiffnessMethod2()
+        {
+            // Set-up model (see Pinned2D - Nodes.txt and Lines.txt)
+
+            // nodes
+            var nodes_temp = new double[,]{
+                {0, 0},
+                {0, 1},
+                {1, 0},
+                {1, 1},
+                {2, 0},
+                {2, 1},
+                {3, 0},
+                {3, 1},
+                {4, 0},
+                {4, 1},
+                {5, 0},
+                {5, 1},
+                {6, 0},
+                {6, 1},
+            };
+            int n = nodes_temp.GetLength(0);
+            Point3D[] nodes = new Point3D[n];
+            for (int i = 0; i < n; i++)
+            {
+                nodes[i] = new Point3D(nodes_temp[i, 0], nodes_temp[i, 1], 0.0);
+            }
+
+            // bars
+            var bars_s = new int[] { 0, 2, 4, 6,  8, 10, 1, 3, 5, 7,  9, 11, 0, 2, 4, 6,  8, 10, 12, 0, 2, 4, 6,  8, 10 };
+            var bars_e = new int[] { 2, 4, 6, 8, 10, 12, 3, 5, 7, 9, 11, 13,  1, 3, 5, 7, 9, 11, 13, 3, 5, 7, 9, 11, 13 };
+            var bars_mat = new double[] { 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 };
+
+            // loads
+            var loads_temp = new double[,] {
+                {0.0, 0.0},
+                {0.0, 0.0},
+                {0.0, 0.0},
+                {0.0, 0.0},
+                {0.0, 0.0},
+                {0.0, 0.0},
+                {0.0, 0.0},
+                {0.0, -1000.0},
+                {0.0, 0.0},
+                {0.0, 0.0},
+                {0.0, 0.0},
+                {0.0, 0.0},
+                {0.0, 0.0},
+                {0.0, 0.0}
+            };
+            Vector3D[] loads = new Vector3D[n];
+            for (int i = 0; i < n; i++)
+            {
+                loads[i] = new Vector3D(loads_temp[i, 0], loads_temp[i, 1], 0.0);
+            }
+
+            // supports
+            var supports_temp = new double[,] {
+                {1, 1},
+                {0, 0},
+                {0, 0},
+                {0, 0},
+                {0, 0},
+                {0, 0},
+                {0, 0},
+                {0, 0},
+                {0, 0},
+                {0, 0},
+                {0, 0},
+                {0, 0},
+                {0, 1},
+                {0, 0}
+            };
+            Vector3D[] supports = new Vector3D[supports_temp.GetLength(0)];
+            for (int i = 0; i < supports.Length; i++)
+            {
+                supports[i] = new Vector3D(supports_temp[i, 0], supports_temp[i, 1], 0.0);
+            }
+
+            // Call the method
+
+            Vector3D[] displacements;
+            Buckminster.Analysis.StiffnessMethod(nodes, bars_s, bars_e, bars_mat, loads, supports, out displacements);
+
+            var target_displacements = new double[] {
+                0.0, 0.0,
+                0.02276444,0.0,
+                0.0024390612,-0.03210214,
+                0.02276444,-0.029663099,
+                0.007317163,-0.059326164,
+                0.020325394,-0.056887124,
+                0.014634298,-0.07191591,
+                0.0154473055,-0.07435493,
+                0.019512393,-0.056074113,
+                0.00813018,-0.058513153,
+                0.021951437,-0.030476114,
+                0.00325209,-0.03291516,
+                0.021951437,0.0,
+                8.130413E-4,-0.0024390498
+            };
+
+            var results = new double[n * 2];
+            for (int i = 0; i < n; i++)
+            {
+                results[i * 2] = displacements[i].X;
+                results[i * 2 + 1] = displacements[i].Y;
+            }
+
+            // Compare results (to a tolerance)
+
+            for (int i = 0; i < n * 2; i++)
+            {
+                Assert.AreEqual(target_displacements[i], results[i], 1E-06);
+            }
+        }
+    }
+}
