1b0232c9d012d5ebf822738e426afa64e8fd748e
Will Pearson <pearswj@gmail.com>
2013-05-16 19:57:15 +0100

    Better surface filling with chebychev component #7

--- Buckminster/Components/ChebychevComponent.cs
+++ Buckminster/Components/ChebychevComponent.cs
@@ -5,6 +5,8 @@ using Grasshopper.Kernel;
 using Rhino.Geometry;
 using Rhino.Geometry.Intersect;
 
+using System.Windows.Forms;
+
 namespace Buckminster.Components
 {
     public class ChebychevComponent : GH_Component
@@ -17,6 +19,7 @@ namespace Buckminster.Components
                 "Constructs a quad mesh from a NURBS surface using the Chebychev net method.",
                 "Buckminster", "Utils")
         {
+            this.ValuesChanged();
         }
 
         /// <summary>
@@ -130,31 +133,63 @@ namespace Buckminster.Components
 
 
             // now that we have the axes, start to build up the mesh quads in between
+            GH_PreviewUtil preview = new GH_PreviewUtil(GetValue("Animate", false));
             Rhino.Geometry.Mesh mesh = new Rhino.Geometry.Mesh(); // target mesh
             for (int k = 0; k < 4; k++)
             {
+                int k0 = (k + 1) % 4;
+                int padding = 10;
                 Rhino.Geometry.Mesh qmesh = new Rhino.Geometry.Mesh(); // local mesh for quadrant
-                Point3d[,] quad = new Point3d[axis[k].Count, axis[(k + 1) % 4].Count]; // 2d array of points
-                int[,] qindex = new int[axis[k].Count, axis[(k + 1) % 4].Count]; // 2d array of points' indices in local mesh
+                Point3d[,] quad = new Point3d[axis[k].Count + padding, axis[k0].Count + padding]; // 2d array of points
+                int[,] qindex = new int[axis[k].Count + padding, axis[k0].Count + padding]; // 2d array of points' indices in local mesh
                 int count = 0;
-                for (int i = 0; i < quad.GetLength(1); i++)
+                for (int i = 0; i < axis[k0].Count; i++)
                 {
                     // add axis vertex to mesh and store point and index in corresponding 2d arrays
-                    quad[0, i] = axis[(k + 1) % 4][i];
-                    qmesh.Vertices.Add(axis[(k + 1) % 4][i]);
+                    quad[0, i] = axis[k0][i];
+                    qmesh.Vertices.Add(axis[k0][i]);
                     qindex[0, i] = count++;
                 }
 
                 for (int i = 1; i < quad.GetLength(0); i++)
                 {
-                    // add axis vertex
-                    quad[i, 0] = axis[k][i];
-                    qmesh.Vertices.Add(axis[k][i]);
-                    qindex[i, 0] = count++;
+                    if (i < axis[k].Count)
+                    {
+                        // add axis vertex
+                        quad[i, 0] = axis[k][i];
+                        qmesh.Vertices.Add(axis[k][i]);
+                        qindex[i, 0] = count++;
+                    }
                     // for each column attempt to locate a new vertex in the grid
                     for (int j = 1; j < quad.GetLength(1); j++)
                     {
-                        if (quad[i - 1, j] == null || quad[i, j - 1] == null) { break; } // quit if neighbouring vertices don't exist
+                        // if quad[i - 1, j] doesn't exist, try to add it and continue (or else break the current row)
+                        if (quad[i - 1, j] == new Point3d())
+                        {
+                            if (j < 2) { break; }
+                            CurveIntersections isct = this.ArcIntersect(S, quad[i - 1, j - 1], quad[i - 1, j - 2], R);
+                            if (isct.Count > 0)
+                            {
+                                quad[i - 1, j] = isct[0].PointB;
+                                qmesh.Vertices.Add(quad[i - 1, j]);
+                                qindex[i - 1, j] = count++;
+                            }
+                            else
+                                break;
+                        }
+                        // if quad[i, j - 1] doesn't exist, try to create quad[i, j] by projection and skip mesh face creation
+                        if (quad[i, j - 1] == new Point3d())
+                        {
+                            if (i < 2) { break; }
+                            CurveIntersections isct = this.ArcIntersect(S, quad[i - 1, j], quad[i - 2, j], R);
+                            if (isct.Count > 0)
+                            {
+                                quad[i, j] = isct[0].PointB;
+                                qmesh.Vertices.Add(quad[i, j]);
+                                qindex[i, j] = count++;
+                                continue;
+                            }
+                        }
                         // construct a sphere at each neighbouring vertex ([i,j-1] and [i-1,j]) and intersect
                         Sphere sph1 = new Sphere(quad[i, j - 1], R);
                         Sphere sph2 = new Sphere(quad[i - 1, j], R);
@@ -169,14 +204,20 @@ namespace Buckminster.Components
                             {
                                 if ((ie.PointA - quad[i - 1, j - 1]).Length > 0.2 * R) // compare with a tolerance, rather than exact comparison
                                 {
-                                    Point3d point = ie.PointA;
-                                    quad[i, j] = point;
-                                    qmesh.Vertices.Add(point);
+                                    quad[i, j] = ie.PointA;
+                                    qmesh.Vertices.Add(quad[i, j]);
                                     qindex[i, j] = count++;
                                     // create quad-face
                                     qmesh.Faces.AddFace(qindex[i, j], qindex[i - 1, j], qindex[i - 1, j - 1], qindex[i, j - 1]);
                                     break;
                                 }
+                            }
+                            if (preview.Enabled)
+                            {
+                                preview.Clear();
+                                preview.AddMesh(mesh);
+                                preview.AddMesh(qmesh);
+                                preview.Redraw();
                             }
                         }
                     }
@@ -192,6 +233,8 @@ namespace Buckminster.Components
 
 
             DA.SetData(0, mesh);
+
+            preview.Clear();
         }
 
         /// <summary>
@@ -213,6 +256,48 @@ namespace Buckminster.Components
         public override Guid ComponentGuid
         {
             get { return new Guid("{2cfe6201-8101-465c-8739-fefe111f66d2}"); }
+        }
+
+        public override void AppendAdditionalMenuItems(System.Windows.Forms.ToolStripDropDown menu)
+        {
+            //base.AppendAdditionalMenuItems(menu);
+            ToolStripMenuItem toolStripMenuItem = Menu_AppendItem(menu, "Animate", new EventHandler(this.Menu_AnimateClicked), true, GetValue("Animate", false));
+            toolStripMenuItem.ToolTipText = "Preview the mesh construction process.";
+        }
+
+        private void Menu_AnimateClicked(Object sender, EventArgs e)
+        {
+            //RecordUndoEvent("Name Of Undo");
+            SetValue("Animate", !this.GetValue("Animate", false));
+            ExpireSolution(true);
+        }
+
+        protected override void ValuesChanged()
+        {
+            if (this.GetValue("Animate", false))
+                this.Message = "Animate";
+            else
+                this.Message = null;
+        }
+
+        /// <summary>
+        /// A helper function to find the next point by walking out in a direction and pivoting down onto the surface.
+        /// Similar approach to initial axis generation.
+        /// </summary>
+        private CurveIntersections ArcIntersect(Surface s, Point3d pt, Point3d pt_prev, double rad)
+        {
+            double u, v;
+            // get uv
+            s.ClosestPoint(pt, out u, out v);
+            // get normal for uv
+            Vector3d n = s.NormalAt(u, v);
+            // scale normal by desired length
+            n *= rad;
+            // create forward facing arc and find intersection point with surface (as uv)
+            // use pt_prev to get direction (middle parameter)
+            Arc arc = new Arc(pt + n, pt + (pt - pt_prev), pt - n);
+            // return intersection(s)
+            return Rhino.Geometry.Intersect.Intersection.CurveSurface(arc.ToNurbsCurve(), s, 0.01, 0.01);
         }
     }
 }
